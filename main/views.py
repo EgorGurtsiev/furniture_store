41b49c11f7c012568af3cb2fb4f510695a826d19
EgorGurtsiev <egor.gurtsiev@gmail.com>
2025-05-28 06:36:11 +1000

    Товары добавлены на главную страницу

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 
+from goods.models import Product
+
 
 def under_construction(request, *args, **kwargs):
     return render(request, 'main/503.html', status=503)
@@ -10,5 +12,6 @@ def under_construction(request, *args, **kwargs):
 def index(request):
     context = {
         'title': 'NailFurni',
+        'products': Product.objects.all().with_main_image().with_current_price()
     }
     return render(request, 'main/index.html', context)
